3e473e505ae46874c6b681c2660a0e00d95ec2d2
shimasaberianpour1375-gif <shimasaberianpour1375@gmail.com>
2025-11-12 00:11:23 +0000

    Update fct_questions.sql
    Third

--- models/core/fct_questions.sql
+++ models/core/fct_questions.sql
@@ -3,11 +3,12 @@
 select
   q.question_id,
   cast(q.date_key as date)       as date_key,
-  q.month_key,                   -- keep the original YYYY-MM string
+  q.month_key,                   
   q.answered_flag,
   q.answer_count,
   q.score,
   q.view_count
 from {{ ref('int_questions_enriched') }} q
 where q.date_key >= date('2017-01-01')
-  and q.date_key <= date('2022-09-25')   -- your real max date
+  and q.date_key <= date('2022-09-25')   
+
